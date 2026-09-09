818983a8dab34fccdbfb5c03bc5a55e768e55e0c
rithumary <rithumary001@gmail.com>
2026-09-09 10:51:18 +0530

    student result management files

---
+++ student.py
@@ -0,0 +1,13 @@
+class student:
+    def __init__(self):
+        self.id=0
+        self.name=""
+        self.age=0
+        self.course=""
+    def add_student(self):
+        pass
+    def display_student(self):
+        pass
+obj_student=student()
+obj_student.add_student()
+obj_student.display_student()
